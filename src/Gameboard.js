1c40cf8decb367d0850149f9f7fe1abf848b932e
Atharva Pawar <164200355+Atharvapawar007@users.noreply.github.com>
2026-09-03 23:53:41 +0000

    feat: implement allShipsSunk() function

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -14,6 +14,9 @@ function Gameboard() {
     //ships
     const ships = [];
 
+    //map to trace from coordinates to a ship
+    const map = new Map();
+
     function placeShip(ship, start, orientation){
         const length = ship.length;
         const [x, y] = start;
@@ -21,23 +24,36 @@ function Gameboard() {
         if(orientation === "horizontal"){
             for(let j = y; j < y + length; j++){
                 board[x][j] = 1;
+                map.set(`${[x, j]}`, ship);
                 ship.addCoordinates([x, j]);
             }
         }else{
             for(let i = x; i < x + length; i++){
                 board[i][y] = 1;
+                map.set(`${[i, y]}`, ship);
                 ship.addCoordinates([i, y]);
             }
         }
 
         ships.push(ship);
+
+    }
+
+    function allShipsSunk(){
+        for(const ship of ships){
+            if(!ship.isSunk()){
+                return false;
+            }
+        }
+        return true;
     }
 
     return {
         get ships(){
             return ships;
         },
-        placeShip
+        placeShip,
+        allShipsSunk
     }
 }
 
